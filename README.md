513c171138031b7f4589c4224b0a07f6168aa635
metablog <khu10735@naver.com>
2023-03-11 10:05:14 +0900

    checkbox_delete

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-npm uuid 로 고요한 아이디 만들기 npm install uuid
-import { v4 as uuid4 } from 'uuid';
-{ id: uuid4(),}
+1. npm uuid 로 고요한 아이디 만들기 npm install uuid
+   import { v4 as uuid4 } from 'uuid';
+   { id: uuid4(),}
+2. checkbox delete 기능추가
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
@@ -7,11 +8,20 @@ export default function TodoList() {
     { id: '124', text: 'darsqilish', status: 'active' },
   ]);
   const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
   return (
     <section>
       <ul>
         {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
       <AddTodo onAdd={handleAdd} />
